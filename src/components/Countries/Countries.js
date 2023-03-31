56d321330fcedbd05ad47c8768b2fdd324828fd5
ualiurrahat <ualiurrahat25@gmail.com>
2023-03-31 23:26:13 +0600

    passed multiple data with to a child component

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -4,15 +4,15 @@ import Country from '../Country/Country';
 const Countries = () => {
     // 5 steps to load data
     // 1. declare state to save loaded data
-    const [countries,setCountries] = useState([]);
+    const [countries, setCountries] = useState([]);
     // 2. write useEffect()
-    useEffect( () =>{
+    useEffect(() => {
         // 3. write fetch() inside useEffect() & set loaded data
         fetch("https://restcountries.com/v3.1/all")
-        .then(res =>res.json())
-        // 4. connect state with data
-        .then(data => setCountries(data))
-    } , []);
+            .then(res => res.json())
+            // 4. connect state with data
+            .then(data => setCountries(data))
+    }, []);
     return (
         <div>
             <h1>Welcome To Every Country in The World!</h1>
@@ -22,10 +22,9 @@ const Countries = () => {
             }
             {
                 countries.map(country => <Country
-                     name = {country.name.common}
-                     population = {country.population}
-                     area = {country.area}
-                     ></Country>)
+                    country={country}
+
+                ></Country>)
             }
         </div>
     );
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './Country.css'
 
 const Country = (props) => {
+    // destructuring the object 'country'
+    const {name,population,area,region} = props.country;
     return (
         <div className='country'>
-            <h2>Country:{props.name}</h2>
-            <p>Population: {props.population}</p>
-            <p>Area: {props.area}</p>
+            <h2>Country:{name.common}</h2>
+            <p>Population: {population}</p>
+            <p>Area: {area}</p>
+            <p>Region: {region}</p>
         </div>
     );
 };
