5ba179a8a1f303993384afabe7e7aaa77371e44b
ualiurrahat <ualiurrahat25@gmail.com>
2023-03-31 15:11:11 +0600

    loaded data in a simple way

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadCountries></LoadCountries>
     </div>
   );
 }
 
+function LoadCountries() {
+  // 5 steps to load data:
+  // 1. declaring state to save loaded data
+  const [countries,setCountries] = useState([]);
+  // 2. writing useEffect()
+  useEffect( () =>{
+    // 3. writing fetch() inside useEffect() & setting loaded data
+   fetch("https://restcountries.com/v3.1/all")
+   .then(res =>res.json())
+   .then(data =>setCountries(data))
+  },[])
+  return (
+    <div>
+      <h1>Visiting Every Countrie of The Whold World!!!</h1>
+      <h3>Total Countries: {countries.length}</h3>
+     {
+      countries.map(country => <Country name = {country.name.common} population = {country.population}></Country>)
+     }
+    </div>
+
+  );
+
+
+}
+
+function Country(props)
+{
+  return (
+    <div>
+      <h2>Name: {props.name}</h2>
+      <p>Population:{props.population}</p>
+    </div>
+  );
+}
 export default App;
